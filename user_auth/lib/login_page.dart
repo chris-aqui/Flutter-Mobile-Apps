91a348d64c15add6c2db2c02d397385a8da809ee
Chris <aqui.christine@gmail.com>
2018-09-20 17:52:33 -0400

    added validation but onlly checks in inputs are empty or not

--- user_auth/lib/login_page.dart
+++ user_auth/lib/login_page.dart
@@ -8,33 +8,44 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage>{
 
+  final formKey = new GlobalKey<FormState>();
+
   String _email;
   String _password;
 
   void validationAndSave(){
-
+    final form = formKey.currentState;
+    if (form.validate()) {
+      print('Form is valid');
+    } else {
+      print('Form is not valid');
+    }
   }
 
   @override
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
+        // title of the appBar
         title: new Text('User Login'),
       ),
+//      the actual body of the app
       body:  new Container(
+//        this keeps the input fields from stretching from end to end... maakes it look nice
         padding: EdgeInsets.all(16.0),
         child: new Form(
+          key: formKey,
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               new TextFormField(
                 decoration: new InputDecoration(labelText: 'Email'),
-                validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null
+                validator: (val) => val.isEmpty ? 'Email can\'t be empty.' : null,
               ),
               new TextFormField(
                 decoration: new InputDecoration(labelText: 'Password'),
-                validator: (value) => value.isEmpty ? 'password can\'t be empty' : null
                 obscureText: true,
+                validator: (val) => val.isEmpty ? 'Password can\'t be empty.' : null,
               ),
               new RaisedButton(
                 child: new Text('Login',
